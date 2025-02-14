05b2cd5281f74370eab64ff007e5e52771284366
lenok-ar <leno4ka279a@gmail.com>
2025-02-15 03:44:00 +0700

    Add files via upload

---
+++ Task 1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void firstProgram()
+    {
+        Console.Write("Введите число которое будут возводить в степень: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите значение степени: ");
+        int power = Convert.ToInt32(Console.ReadLine());
+        int numberRaisedToAPower = number; // число возведенное в степень
+
+        for (int i = 1; i < power; i++)
+        {
+            numberRaisedToAPower = numberRaisedToAPower * number;
+        }
+        Console.WriteLine($"Результат: {numberRaisedToAPower}");
+    }
+
+    static void secondProgram()
+    {
+        Console.Write("Введите число содержащее от трех до шести цифр (включительно): ");
+
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        if (firstNumber < 100 & firstNumber > 1000000)
+        {
+            Console.Write("Число содержит меньше трех цифр или больше шести");
+        }
+
+        if (firstNumber > 100 & firstNumber < 999)
+        {
+            int secondDigit = (firstNumber / 10) % 10;
+            int CombineTheNumber = (firstNumber / 100) * 10 + (firstNumber % 10);
+            int finalNumber = CombineTheNumber * 10 + secondDigit;
+
+            Console.WriteLine($"Результат: {finalNumber}");
+        }
+
+        if (firstNumber > 1000 & firstNumber < 9999)
+        {
+            int secondDigit = (firstNumber / 100) % 10; // находим вторую цифру в числе
+            int CombineTheNumber = (firstNumber / 1000) * 100 + (firstNumber % 100); // объединяем все оставшиеся числа в одно, удаляем вторую цифру
+            int finalNumber = CombineTheNumber * 10 + secondDigit;
+
+            Console.WriteLine($"Результат: {finalNumber}");
+        }
+
+        if (firstNumber > 10000 & firstNumber < 99999)
+        {
+            int secondDigit = (firstNumber / 1000) % 10;
+            int CombineTheNumber = (firstNumber / 10000) * 1000 + (firstNumber % 1000);
+            int finalNumber = CombineTheNumber * 10 + secondDigit;
+
+            Console.WriteLine($"Результат: {finalNumber}");
+        }
+
+        if (firstNumber > 100000 & firstNumber < 999999)
+        {
+            int secondDigit = (firstNumber / 10000) % 10;
+            int CombineTheNumber = (firstNumber / 100000) * 10000 + (firstNumber % 10000);
+            int finalNumber = CombineTheNumber * 10 + secondDigit;
+
+            Console.WriteLine($"Результат: {finalNumber}");
+        }
+    }
+
+    static void Main()
+    {
+        string choice;
+
+        while (true)
+        {
+            Console.Write("Выберите задание (для работы первого задания введите 1, для работы второго задания - 2): ");
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    firstProgram();
+                    break;
+                case "2":
+                    secondProgram();
+                    break;
+            }
+        }
+    }
+}
